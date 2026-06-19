b8458081d448052b68487757763fdbec9c4311c7
Prince <princekumarsingh016592@gmail.com>
2026-06-19 14:57:36 +0530

    Practice Unit -1 complete

--- practice.cpp
+++ practice.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
 using namespace std;
 
+void avg(int a, int b, int c){
+    cout << "Average of three numbers is: " << (a+b+c)/3 << endl;
+}
+
+void largestofthree(int a, int b, int c){
+  if(a>b && a>c){
+    cout << "Largest number is:" << a << endl;
+  }
+  else if(b>a && b>c){
+    cout << "Largest number is:" << b << endl;
+  }
+  else{
+    cout << "Largest number is:" << c << endl;
+  }
+}
+
+void areaofrect(int a, int b){
+  cout << "Area of rectangle is:" << a*b << endl;
+}
+
+void evenodd(int a){
+  if(a%2==0){
+    cout << "Number is even" << endl;
+  }
+  else{
+    cout << "Number is odd" << endl;
+}
+}
+
+void multiple(int a=3){
+  for(int i=10; i<=50; i++){
+    cout << a*i << endl;
+  }
+}
+
+
 int main() {
-    int l, w, area;
-
-    cout << "Enter length of rectangle: " << endl;
-    cin >> l;
-
-    cout << "Enter width of rectangle: " << endl;
-    cin >> w;
-
-    area = l * w;
-
-    cout << "Area of rectangle is: " << area << endl;
-
+    avg(1,2,3);
+    largestofthree(1,2,3);
+    areaofrect(1,2);
+    evenodd(1);
+    multiple();
     return 0;
 }
