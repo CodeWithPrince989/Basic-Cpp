0509bcc94bdf96c3b26ae6315ae7f79998f714c3
Prince <princekumarsingh016592@gmail.com>
2026-06-22 11:12:13 +0530

    Anagram  in CPP

--- anagram.cpp
+++ anagram.cpp
@@ -1,30 +1,26 @@
-#include<iostream>
-#include<string>
-using namespace  std;
+class Solution {
+public:
+    bool isAnagram(string str1, string str2) {
+        if(str1.length() != str2.length()) {
+            return false;
+        }
 
-bool isAnagram(string str1, string str2){
-  if(str1.length() != str2.length()){
-    return false;
-  }
-  int count[26] = {0};
-  for(int i=0; i<str1.length(); i++){
-    int idx = str1[i] - 'a';
-    cout[idx]++;
-  }
-  for(int i=0; i<str2.length(); i++){
-    int idx = str2[i] - 'a';
-    
-    if(count[idx] < 0){
-      return false;
+        int count[26] = {0};
+
+        for(int i = 0; i < str1.length(); i++) {
+            int idx = str1[i] - 'a';
+            count[idx]++;
+        }
+
+        for(int i = 0; i < str2.length(); i++) {
+            int idx = str2[i] - 'a';
+            count[idx]--;
+
+            if(count[idx] < 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
-    cout[idx]--;
-  }
-  return true;
-}
-
-int main(){
-  string str1 = "anagram";
-  string str2 = "nagaram";
-  isAnagram(str1, str2);
-  return 0;
-}
+};
